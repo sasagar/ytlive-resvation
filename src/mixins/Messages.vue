e3489d8b35e834d8aec2a23cc9ea19303d15825a
SASAGAWA Kiyoshi <sasagawa@kent-and-co.com>
2021-02-23 02:52:08 +0900

    add support for Reserve from Chat #6

--- src/mixins/Messages.vue
+++ src/mixins/Messages.vue
@@ -5,6 +5,7 @@ const socket = io("http://localhost:8081");
 export default {
   methods: {
     sendChat(message) {
+      console.log("mixins/Messages/sendChat");
       socket.emit("sendReserveMessage", message);
     }
   }
--- src/views/LiveDashboard.vue
+++ src/views/LiveDashboard.vue
@@ -362,7 +362,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .dashboard {
   height: 100%;
 }
